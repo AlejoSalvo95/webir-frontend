8d65bca22e739f71eaf26e75903c56f15c8dd2ce
Alejo Salvo <alejo.salvo@abstracta.com.uy>
2019-11-14 16:17:46 -0300

    Add groupBy function

--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -2,6 +2,26 @@ import phoneSvg from "./Assets/Images/phone.svg";
 import appleSvg from "./Assets/Images/apple.svg";
 import samsungSvg from "./Assets/Images/samsung.svg";
 
+// let phones = [{model: "iphone 6", page: "zonalaptop", price:9}, {model: "iphone 6", page: "zonaTecno", price:8}, {model: "iphone 7", page: "zonaTecno", price:7}]
+
+let keyGetter = item => item.model
+export const groupBy = (list, keyGetter) => {
+  let groupedPhones = {};
+  list.forEach((item) => {
+    const key = keyGetter(item);
+    console.log(key)
+    console.log(groupedPhones[key])
+    if (!groupedPhones[key] || !groupedPhones[key].length) {
+      groupedPhones[key] =[item];
+    } else {
+      groupedPhones[key].push(item);
+    }
+  });
+}
+
+// groupBy(phones, keyGetter)
+
+
 export const phone = {
   text: "Phone",
   imgSrc: phoneSvg
